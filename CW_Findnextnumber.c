b247cf1496577179b3b37b963c4e45cb1c4fbf1d
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-06-16 18:06:44 +0200

    works
    just needs some optimalization

--- CW_Findnextnumber.c
+++ CW_Findnextnumber.c
@@ -3,16 +3,23 @@ int numberOf1;
 int highNumber;
 int highNumberof1;
 int next_higher(int n) {
+    if(n == 0){
+        return n;
+    }
+    if(n == 2147483647){
+        return n;
+    }
     numberOf1 = 0;
     highNumber = n + 1;
     highNumberof1 = 0;
-    for (int i = 31; i >= 0; i--) {
+    for (int i =  31; i >= 0; i--) {
         int k = n >> i;
         if (k & 1){
             numberOf1++;
         }
     }
     while(highNumberof1 != numberOf1){
+        highNumberof1 = 0;
         for (int i = 31; i >= 0; i--) {
             int k = highNumber >> i; 
             if (k & 1) 
@@ -22,12 +29,12 @@ int next_higher(int n) {
             return highNumber;
         }
         highNumber++;
-        highNumberof1 = 0;
+        
     }
-    return 0;
+    return n;
 }
 
 int main(void){
-    int actual = next_higher(128868543);
+    int actual = next_higher(2147483647);
     printf("%d", actual);
 }
